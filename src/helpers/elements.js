012459cdabe785dec5b7a237e660bc0c4689e3a4
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-08-04 22:36:26 -0400

    custom create element function

---
+++ src/helpers/elements.js
@@ -0,0 +1,14 @@
+function createElement(type, id = '', classes = []) {
+  if (!type) throw new Error('Element type required.');
+  if (id && typeof id !== 'string') throw new Error('id must be type string');
+  if (classes.length !== 0 && !Array.isArray(classes)) throw new Error('classes must be an array.');
+
+  const element = document.createElement(type);
+  if (id) element.id = id;
+  if (classes) {
+    let cl = '';
+    classes.forEach(className => element.classList.add(cl));
+  }
+}
+
+export { createElement };
