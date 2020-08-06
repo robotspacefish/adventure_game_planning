0354cccd8f15cdde09adce9983732ffc0450d6ec
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-08-05 23:05:04 -0400

    map class

---
+++ src/map/map.js
@@ -0,0 +1,19 @@
+class Map {
+    constructor(rows, cols) {
+        this._rows = rows;
+        this._cols = cols;
+        this.init(layout);
+    }
+
+    get rows() {
+        return this._rows;
+    }
+
+    get cols() {
+        return this._cols;
+    }
+
+    init() {
+        // set up map 
+    }
+}
