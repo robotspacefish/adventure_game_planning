5fa5603eaffb262eaf132aa9484f8d2ad6f51f30
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-08-07 16:06:38 -0400

    add textContent

--- src/helpers/elements.js
+++ src/helpers/elements.js
@@ -1,13 +1,20 @@
-function createElement(type, id = '', classes = []) {
+function createElement(type, id = '', classes = [], textContent = '') {
   if (!type) throw new Error('Element type required.');
   if (id && typeof id !== 'string') throw new Error('id must be type string');
   if (classes.length !== 0 && !Array.isArray(classes)) throw new Error('classes must be an array.');
+  if (textContent && typeof textContent !== 'string') throw new Error('id must be type string');
 
   const element = document.createElement(type);
+
   if (id) element.id = id;
+
   if (classes) {
     classes.forEach(className => element.classList.add(className));
   }
+
+  if (textContent) element.textContent = textContent;
+
+  return element;
 }
 
 export { createElement };
