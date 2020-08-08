7e8ef40fe4e34f55ba3f05af6d4e1e48f3266dbe
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-08-07 23:31:49 -0400

    use init method to select screen by type/mode

--- src/screen/screen.js
+++ src/screen/screen.js
@@ -2,8 +2,9 @@ import './screen.css';
 import { createElement } from '../helpers/elements';
 
 class Screen {
-  constructor(parent) {
+  constructor(parent, type) {
     this._gameTextOutput = '';
+    this._type = type;
     this._screenContainer = createElement('div', 'js-screen-container');
     parent.append(this._screenContainer)
   }
@@ -12,11 +13,26 @@ class Screen {
     this._screenContainer.append(child);
   }
 
+  init() {
+    switch (this.type) {
+      case 'title':
+        this.title(arguments);
+        break;
+      default:
+        throw new Error('That screen type does not exist.')
+    }
+  }
+
   get screenContainer() {
     return this._screenContainer;
   }
 
-  title(titleText, startBtnText, callback) {
+  get type() {
+    return this._type;
+  }
+
+  title() {
+    const [titleText, startBtnText, callback] = arguments[0];
     const startBtn = createElement('button', 'js-start-button', 'start-btn btn', startBtnText);
 
     this.appendToContainer(
